42d51b421b94452b510bb53c48eb5ea4a70a1775
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-03-23 20:26:06 +0800

    :sparkles: Save feedback data to the database

--- app/Http/Controllers/SendFeedbackController.php
+++ app/Http/Controllers/SendFeedbackController.php
@@ -15,6 +15,17 @@ class SendFeedbackController extends Controller
         // Validate the request data
         $feedbackData = $request->validated();
 
+        // Save to database
+        $feedback = new Feedback();
+        $feedback->public_id = $this->makePublicId($feedback->id);
+        $feedback->name = data_get($feedbackData, 'name');
+        $feedback->email = data_get($feedbackData, 'email');
+        $feedback->message = data_get($feedbackData, 'message');
+        $feedback->app_info = data_get($feedbackData, 'app_info');
+        $feedback->device_info = data_get($feedbackData, 'device_info');
+        $feedback->additional_info = data_get($feedbackData, 'additional_info');
+        $feedback->save();
+
         // Check is feedbackData 'email' is valid email and not null, send email
         if (isset($feedbackData['email'])) {
             Mail::to($feedbackData['email'])
@@ -29,5 +40,18 @@ class SendFeedbackController extends Controller
         // Process the feedback (e.g., save to database, send email, etc.)
         // For demonstration purposes, we'll just return a success response
         return response()->json(['message' => 'Feedback sent successfully!'], 200);
+    }
+
+    /**
+     * Generate a random string
+     *
+     * @param int $id
+     * @return string
+     */
+    private function makePublicId($id)
+    {
+        // Generate a random alphanumeric uppercase string of length 5
+        $randomString = strtoupper(\Illuminate\Support\Str::random(5));
+        return "MPT-{$randomString}";
     }
 }
