5025d4ecae395fcade842a71c9f99b6ece3768ab
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2025-03-28 05:52:15 +0800

    :passport_control: Implement FilamentUser interface and add panel access control for specific email

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +47,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * https://filamentphp.com/docs/3.x/panels/users
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Right now, hardcode which user can access the panel
+        return str_ends_with($this->email, 'iqmal3@outlook.com');
     }
 }
